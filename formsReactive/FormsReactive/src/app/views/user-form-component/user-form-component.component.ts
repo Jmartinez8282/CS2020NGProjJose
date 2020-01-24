39f07517acf120bc255f411a201387fe00912d9c
Code <code@COCTL-41256.local>
2020-01-24 12:39:33 -0800

    cp

--- formsReactive/FormsReactive/src/app/views/user-form-component/user-form-component.component.ts
+++ formsReactive/FormsReactive/src/app/views/user-form-component/user-form-component.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl,} from '@angular/forms';
+import { FormGroup, FormControl,Validators, FormBuilder,} from '@angular/forms';
 
 
 @Component({
@@ -9,15 +9,66 @@ import { FormGroup, FormControl,} from '@angular/forms';
 })
 export class UserFormComponentComponent implements OnInit {
   userForm: FormGroup;
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+  get fName() { return this.userForm.get('fName'); }
+  get lName() { return this.userForm.get('lName'); }
+  get mName() { return this.userForm.get('mName'); }
 
   ngOnInit() {
-    this.userForm = new FormGroup({
-      fName: new FormControl(''),
-      lName: new FormControl(''),
-      mName: new FormControl(''),
-      hasMiddleName: new FormControl(false)
+
+  
+    //this.UserFormComponentComponent = new FormGroup({
+      //fName: new FormControl('', Validators.required),
+      //lName: new FormControl('',[Validators.required,Validators.minLength(3)]),
+      //mName: new FormControl('',Validators.required),
+      //hasMiddleName: new FormControl(false)
+    //});
+    this.userForm = this.formBuilder.group({
+      fName:['', Validators.required],
+      lName: ['',[Validators.required,Validators.minLength(3)]],
+    mName: ['',Validators.required],
+  hasMiddleName:false 
+     
+  
+});
+
+this.userForm.get('hasMiddleName').valueChanges.subscribe(val => {
+  const control =  this.userForm.get('mName');
+  if(val) {
+    control.clearValidators();
+  } else {
+    control.setValidators(Validators.required);
+  }
+  control.updateValueAndValidity();
+});
+
+  }
+  myUser = { fName: 'Casey', lName: 'Pampy',mName: 'secret', hasMiddleName:true};
+  setValue() {
+    this.userForm.setValue({
+      fName: 'Cassandra',
+      lName: 'Pamplona',
+      mName: 'Casey',
+      hasMiddleName: true
     });
   }
 
+  patchValue(myUser){
+    this.userForm.patchValue({
+      fName: 'Casey'
+    });
+  }
+setValue2(myUser){
+  this.userForm.setValue(myUser)
+
+}
+submitForm() {
+  if(this.userForm.invalid) {
+    alert('Fix errors on form');
+  } else {
+    alert('Succesful!');
+    this.userForm.reset();
+    console.log(this.userForm.value);
+  }
+}
 }
